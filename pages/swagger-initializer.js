058ab55e4c2ade58e2525db1499065a7b78a4af8
SeongHo5 <newfuring1123@gmail.com>
2025-02-02 13:22:49 +0900

    title, favicon 수정

--- pages/swagger-initializer.js
+++ pages/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
-        urls: [{url: "./openapi/schemas.yaml", name: "Common Schema"}, {url: "./openapi/v1_user.yaml", name: "User API"}, {url: "./openapi/v1_auth.yaml", name: "Auth API"}, {url: "./openapi/v1_found.yaml", name: "Found Item API"}, {url: "./openapi/v1_lost.yaml", name: "Lost Item API"}, {url: "./openapi/v1_common.yaml", name: "Common API"}, {url: "./openapi/v1_admin.yaml", name: "Admin API"}],
+        urls: [{url: "./openapi/schemas.yaml", name: "Common Schema"}],
         dom_id: '#swagger-ui',
         deepLinking: true,
         docExpansion: 'full',
