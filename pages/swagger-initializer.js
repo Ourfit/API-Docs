5c8fd4b40e4e3b287415c32d4945f0acad1a9e28
SeongHo5 <newfuring1123@gmail.com>
2025-02-24 22:17:19 +0900

    목록에 API 버전 명시 추가

--- pages/swagger-initializer.js
+++ pages/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
-        urls: [{url: "./openapi/schemas.yaml", name: "Common Schema"}, {url: "./openapi/v1_auth.yaml", name: "Auth API"}, {url: "./openapi/v1_user.yaml", name: "User API"}, {url: "./openapi/v1_challenge.yaml", name: "Challenge API"}, {url: "./openapi/v1_mate.yaml", name: "Mates API"}, {url: "./openapi/v1_common.yaml", name: "Common API"}],
+        urls: [{url: "./openapi/schemas.yaml", name: "Common Schema"}, {url: "./openapi/v1_auth.yaml", name: "Auth API (v1)"}, {url: "./openapi/v1_user.yaml", name: "User API (v1)"}, {url: "./openapi/v1_challenge.yaml", name: "Challenge API (v1)"}, {url: "./openapi/v1_mate.yaml", name: "Mates API (v1)"}, {url: "./openapi/v1_common.yaml", name: "Common API (v1)"}],
         dom_id: '#swagger-ui',
         deepLinking: true,
         docExpansion: 'full',
